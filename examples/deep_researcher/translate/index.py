5c62ab43d6020d0dbc900528cdf5771aeffe6c74
bernatsampera <b.sampera@gmail.com>
2025-08-22 08:44:00 +0200

    feat: improve supervisor, make it below the initial trasnlation

--- examples/deep_researcher/translate/index.py
+++ examples/deep_researcher/translate/index.py
@@ -2,7 +2,9 @@ from typing import Annotated, Literal
 
 from langchain.chat_models import init_chat_model
 from langchain_core.messages import (
+    AIMessage,
     BaseMessage,
+    HumanMessage,
     get_buffer_string,
 )
 from langgraph.graph import START, MessagesState, StateGraph, add_messages
@@ -53,80 +55,10 @@ class TranslateState(TranslateInputState):
 llm = init_chat_model(model="google_genai:gemini-2.5-flash-lite")
 
 
-# def translate(state: TranslateState) -> Command[Literal["__end__"]]:
-#     """Translate the messages to the user."""
-#     translate_iterations = state.get("translate_iterations", 0)
-#     prompt = ""
-
-#     if translate_iterations == 0:
-#         state["original_text"] = state["messages"][-1].content
-#         found_glossary_words = match_words_from_glossary(
-#             glossary_en_es, state["original_text"]
-#         )
-#         state["words_to_match"] = found_glossary_words
-
-#         prompt = first_translation_instructions.format(
-#             text=state["original_text"],
-#             translation_instructions=translation_instructions.format(
-#                 glossary=format_glossary(found_glossary_words),
-#             ),
-#         )
-#     else:
-#         last_two_messages = state["messages"][-2:]
-#         prompt = improve_translation_instructions.format(
-#             messages=get_buffer_string(last_two_messages),
-#             translation_instructions=translation_instructions.format(
-#                 glossary={},  # Glossary can be empty because we are just fixing based on input from the user
-#             ),
-#         )
-
-#     response = llm.invoke(prompt)
-
-#     return Command(
-#         goto=END,
-#         update={
-#             "messages": [response],
-#             "original_text": state["original_text"],
-#             "translate_iterations": translate_iterations + 1,
-#             "current_translation": response.content,  # TODO: REMOVE, THIS IS FOR DEBUGGING PURPOSES IN LANGGRAPH STUDIO
-#             "words_to_match": state["words_to_match"],
-#         },
-#     )
+# the fish drink in the river
 
 
-# graph = StateGraph(TranslateState, input_schema=TranslateInputState)
-
-# graph.add_node("translate", translate)
-
-# graph.add_edge(START, "translate")
-
-# graph = graph.compile()
-
-
-# the principal has used the instrument to give the global history class
-
-
-def supervisor(
-    state: TranslateState,
-) -> Command[Literal["initial_translation", "refine_translation"]]:
-    translate_iterations = state.get("translate_iterations", 0)
-    if translate_iterations == 0:
-        return Command(
-            goto="initial_translation",
-            update={
-                "messages": [state["messages"][-1]],
-            },
-        )
-    else:
-        return Command(
-            goto="refine_translation",
-            update={
-                "messages": [state["messages"][-1]],
-            },
-        )
-
-
-def initial_translation(state: TranslateState) -> Command[Literal["wait_for_user"]]:
+def initial_translation(state: TranslateState) -> Command[Literal["supervisor"]]:
     text = state["messages"][-1].content
     state["original_text"] = text
 
@@ -142,16 +74,31 @@ def initial_translation(state: TranslateState) -> Command[Literal["wait_for_user
     response = llm.invoke(prompt)
 
     return Command(
-        goto="wait_for_user",
+        goto="supervisor",
         update={
-            "messages": [response],
-            "current_translation": response.content,
+            "messages": [AIMessage(content=response.content)],
+            "current_translation": HumanMessage(
+                content=response.content
+            ),  # TODO: REMOVE, THIS IS FOR DEBUGGING PURPOSES IN LANGGRAPH STUDIO
             "translate_iterations": 1,
         },
     )
 
 
-def refine_translation(state: TranslateState) -> Command[Literal["wait_for_user"]]:
+def supervisor(
+    state: TranslateState,
+) -> Command[Literal["refine_translation"]]:
+    value = interrupt({"text_to_revise": state["messages"][-1].content})
+    print(value)
+    return Command(
+        goto="refine_translation",
+        update={
+            "messages": [HumanMessage(content=value)],
+        },
+    )
+
+
+def refine_translation(state: TranslateState) -> Command[Literal["supervisor"]]:
     last_two_messages = state["messages"][-2:]
     prompt = improve_translation_instructions.format(
         messages=get_buffer_string(last_two_messages),
@@ -160,23 +107,13 @@ def refine_translation(state: TranslateState) -> Command[Literal["wait_for_user"
     response = llm.invoke(prompt)
 
     return Command(
-        goto="wait_for_user",
-        update={
-            "messages": [response],
-            "current_translation": response.content,
-            "translate_iterations": state["translate_iterations"] + 1,
-        },
-    )
-
-
-def wait_for_user(state: TranslateState) -> Command[Literal["supervisor"]]:
-    value = interrupt({"text_to_revise": state["messages"][-1].content})
-    print(value)
-
-    return Command(
         goto="supervisor",
         update={
-            "messages": [value],
+            "messages": [AIMessage(content=response.content)],
+            "current_translation": HumanMessage(
+                content=response.content
+            ),  # TODO: REMOVE, THIS IS FOR DEBUGGING PURPOSES IN LANGGRAPH STUDIO
+            "translate_iterations": state["translate_iterations"] + 1,
         },
     )
 
@@ -186,8 +123,7 @@ graph = StateGraph(TranslateState, input_schema=TranslateInputState)
 graph.add_node("initial_translation", initial_translation)
 graph.add_node("refine_translation", refine_translation)
 graph.add_node("supervisor", supervisor)
-graph.add_node("wait_for_user", wait_for_user)
 
-graph.add_edge(START, "supervisor")
+graph.add_edge(START, "initial_translation")
 
 graph = graph.compile()
